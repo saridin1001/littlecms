c6725d5aeaf235b3bc0b7f3f44c90767ca1b8097
saridin <saridin@localhost.localdomain>
2022-07-20 20:54:22 +0700

    initial commit

---
+++ app/config/loader.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phalcon\Loader;
+
+$loader = new Loader();
+
+/**
+ * Register Namespaces
+ */
+$loader->registerNamespaces([
+    'Litlecms\Models' => APP_PATH . '/common/models/',
+    'Litlecms'        => APP_PATH . '/common/library/',
+]);
+
+/**
+ * Register module classes
+ */
+$loader->registerClasses([
+    'Litlecms\Modules\Frontend\Module' => APP_PATH . '/modules/frontend/Module.php',
+    'Litlecms\Modules\Cli\Module'      => APP_PATH . '/modules/cli/Module.php'
+]);
+
+$loader->register();
---
+++ app/modules/cli/tasks/VersionTask.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Litlecms\Modules\Cli\Tasks;
+
+class VersionTask extends \Phalcon\Cli\Task
+{
+    public function mainAction()
+    {
+        $config = $this->getDI()->get('config');
+
+        echo $config['version'];
+    }
+}
